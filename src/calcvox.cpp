2dce0398a8e380a8be1fdffb6a374c9343fde41d
Quin <quin.marilyn05@gmail.com>
2023-11-02 12:44:28 -0600

    Convert calcvox.cpp to use speech_handler, but it's still broken.

--- src/calcvox.cpp
+++ src/calcvox.cpp
@@ -1,10 +1,6 @@
 #include <Arduino.h>
 #include <Keypad.h>
-#include "AudioTools.h" // https://github.com/pschatzmann/arduino-audio-tools
-//#include "AudioLibs/AudioKit.h" // https://github.com/pschatzmann/arduino-audiokit
-#include "FileSystems.h" // https://github.com/pschatzmann/arduino-posix-fs
-#include "espeak.h"
-#include "SPI.h"
+#include "speech_handler.h"
 
 const byte ROW_NUM = 3; 
 const byte COLUMN_NUM = 5; 
@@ -52,21 +48,10 @@ char getKey() {
 }
 
 AnalogAudioStream analog ; // or replace with AudioKitStream for AudioKit
-ESpeak espeak(analog);
+speech_handler speech(analog);
+
 void setup() {
   Serial.begin(115200);
-  //file_systems::FSLogger.begin(file_systems::FSInfo, Serial); 
-
-  // setup espeak
-  espeak.begin();
-
-  // setup output
-  audio_info espeak_info = espeak.audioInfo();
-  auto cfg = analog.defaultConfig();
-  cfg.channels = espeak_info.channels; // 1
-  cfg.sample_rate = espeak_info.sample_rate; // 22050
-  cfg.bits_per_sample = espeak_info.bits_per_sample; // 16
-  analog.begin(cfg);
   setupKeypad();
 }
 
@@ -77,6 +62,6 @@ void loop() {
     Serial.println(key);
   }
   if (key == '1') {
-    espeak.say("Hello World");
+    speech.speak("Hello World");
   }
 }
