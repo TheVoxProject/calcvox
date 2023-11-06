9596e906beb875deeafac3b1500ba3d3e9de680e
Quin <quin.marilyn05@gmail.com>
2023-11-06 12:32:39 -0700

    Map keys to mathematical functions and make  a function to convert characters.

--- src/calcvox.cpp
+++ src/calcvox.cpp
@@ -4,6 +4,7 @@
 #include "filesystems.h"
 #include "espeak.h"
 #include "SPI.h"
+#include <string>
 
 const byte ROWS = 3;
 const byte COLUMNS = 5;
@@ -13,9 +14,9 @@ byte cols[COLUMNS] = {23, 22, 14, 32, 15};
 byte rows[ROWS] = {33, 27, 22};
 
 char keys[ROWS][COLUMNS] = {
-	{'1', '2', '3', '4', '5'},
-	{'6', '7', '8', '9', '0'},
-	{'*', '#', 'A', 'B', 'C'}
+	{'0', '1', '4', '7', '+'},
+	{'.', '2', '5', '8', '-'},
+	{'=', '3', '6', '9', '*'}
 };
 
 void setup_keypad() {
@@ -46,6 +47,47 @@ char get_key() {
 	return NO_KEY;
 }
 
+// Todo: clean this up.
+std::string convert_character(const char character) {
+	std::string final;
+	switch (character) {
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+		final = character;
+		break;
+	case '+':
+		final = "plus";
+		break;
+	case '-':
+		final = "minus";
+		break;
+	case '*':
+		final = "times";
+		break;
+	case '/':
+		final = "divide";
+		break;
+	case '.':
+		final = "point";
+		break;
+	case '=':
+		final = "equals";
+		break;
+	default:
+		final = "";
+		break;
+	}
+	return final;
+}
+
 AnalogAudioStream analog ;
 ESpeak espeak(analog);
 
@@ -65,9 +107,7 @@ void loop() {
 	char key = get_key();
 	if (key != NO_KEY) {
 		// Serial.println(key);
-		char to_speak[2];
-		to_speak[0] = key;
-		to_speak[1] = '\0';
+		const char* to_speak = convert_character(key).c_str();
 		espeak.say(to_speak);
 	}
 }
